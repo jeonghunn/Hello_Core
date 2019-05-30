06f505045faa65457f9dcd4136960189325a9326
Junghoon Lee <jeonghunn1@gmail.com>
2019-05-30 21:08:02 +0900

    Structure update

---
+++ Files which exist inside this folder must be in root folder/config.php
@@ -0,0 +1,23 @@
+<?php if(!defined("642979")) exit();
+
+ini_set('memory_limit', '1024M');
+
+$HELLO_CORE_VERSION = "1.0";
+$CORE_VERSION = "3.18.514";
+$DEVELOPMENT_SERVER_URL = "unopenedbox.com/develop/square/";
+$SERVER_URL = "s9uare.com/";
+$MAIN_URL = "http://s9uare.com/";
+$MAIN_API_URL = "http://s9uare.com/api.php";
+$CLIENT_SERVER_IP_ADDRESS = "52.78.110.116";
+
+//DATABASE
+$DB_NAME = "square";
+$DB_SERVER_PATH = "localhost";
+$DB_USERNAME = "root";
+$DB_PASSWORD = "root";
+
+//Import Modules
+$IMPORT_MODULE = array('settings', 'main', 'account', 'board', 'page', 'notification', 'attach', 'square');
+
+
+?>
