df9597ba5f79dc494c8aa00fec2c4a50ecde327e
Junghoon Lee <jeonghunn1@gmail.com>
2019-09-12 02:41:17 +0900

    fix: Fix logger makes server frozen when lot of requests occur

--- logger.php
+++ logger.php
@@ -1,33 +1,37 @@
 <?php if(!defined("642979")) exit();
  //add log       
-       
 
-function ActLog($user_srl, $REMOTE_ADDR, $date, $log_category, $log){
-	     DBQuery("INSERT INTO `log` (`user_srl`, `ip_addr`, `date`, `category`, `value`) VALUES ('$user_srl', '$REMOTE_ADDR', '$date' , '$log_category', '$log');");
+
+function ActLog($user_srl, $REMOTE_ADDR, $date, $log_category, $log)
+{
+    DBQuery("INSERT DELAYED INTO `log` (`user_srl`, `ip_addr`, `date`, `category`, `value`) VALUES ('$user_srl', '$REMOTE_ADDR', '$date' , '$log_category', '$log');");
 }
 
-            function ClientAgentLog($user_srl, $REMOTE_ADDR, $useragent, $date){
-            	$row = mysqli_fetch_array(DBQuery("SELECT * FROM  `clients` WHERE `ip_addr` LIKE '$REMOTE_ADDR' AND  `user_agent` LIKE '$useragent'"));
-            	if($row['ip_addr'] != $REMOTE_ADDR || $row['user_agent'] != $useragent){
-            	DBQuery("INSERT INTO `clients` (`user_srl`, `ip_addr`, `user_agent`, `date`) VALUES ('$user_srl', '$REMOTE_ADDR', '$useragent' , '$date');");
-            }
-}
-
-
-function updateLastAccess($user_srl){
-    if($user_srl <= 0) return false;
-    DBQuery(getSqlUpdateQuery('pages', array('last_access'=> getTimeStamp()), array('srl' => $user_srl), true));
+function ClientAgentLog($user_srl, $REMOTE_ADDR, $useragent, $date)
+{
+    $row = mysqli_fetch_array(DBQuery("SELECT * FROM  `clients` WHERE `ip_addr` LIKE '$REMOTE_ADDR' AND  `user_agent` LIKE '$useragent'"));
+    if ($row['ip_addr'] != $REMOTE_ADDR || $row['user_agent'] != $useragent) {
+        DBQuery("INSERT INTO `clients` (`user_srl`, `ip_addr`, `user_agent`, `date`) VALUES ('$user_srl', '$REMOTE_ADDR', '$useragent' , '$date');");
+    }
 }
 
 
-
-function ActLogSyncTask($user_srl, $REMOTE_ADDR, $date, $log_category, $log){
-	ThreadAct('ActLog', array($user_srl, $REMOTE_ADDR, $date, $log_category, $log));
+function updateLastAccess($user_srl)
+{
+    if ($user_srl <= 0) return false;
+    DBQuery(getSqlUpdateQuery('pages', array('last_access' => getTimeStamp()), array('srl' => $user_srl), true));
 }
 
 
-function ClientAgentLogSyncTask($user_srl){
-	ThreadAct('ClientAgentLog', array($user_srl, getIPAddr(), getUserAgent(), getTimeStamp()));
+function ActLogSyncTask($user_srl, $REMOTE_ADDR, $date, $log_category, $log)
+{
+    ThreadAct('ActLog', array($user_srl, $REMOTE_ADDR, $date, $log_category, $log));
+}
+
+
+function ClientAgentLogSyncTask($user_srl)
+{
+    ThreadAct('ClientAgentLog', array($user_srl, getIPAddr(), getUserAgent(), getTimeStamp()));
 }
 
   
